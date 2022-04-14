442c37dacc44e2afbe47a5343bcc2c6cd4ed88f4
Eugenetucha <fomenko.woody.list@gmail.com>
2022-04-14 22:00:28 +0700

    start

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace lab10.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
